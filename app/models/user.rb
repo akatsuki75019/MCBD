87ec3ba806dbc127dbc9af5ef977954e2792491a
nellotte <nelly.guerin@gmail.com>
2023-12-05 20:00:02 +0100

    create cart after user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,17 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
 
+  after_create :create_user_cart
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def create_user_cart
+    Cart.create(user: self)
+  end
+  
 end
