064bfd8e589e3657d6f57454a4db46b9c726e0d2
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-12-05 18:56:03 +0100

    Delete joint table between wishlist and books because this was not usefull and took more database space

--- app/models/joint_table_wishlist_book.rb
+++
@@ -1,4 +0,0 @@
-class JointTableWishlistBook < ApplicationRecord
-  belongs_to :wishlist
-  belongs_to :book
-end
--- db/migrate/20231205145856_create_wishlists.rb
+++ db/migrate/20231205145856_create_wishlists.rb
@@ -2,6 +2,7 @@ class CreateWishlists < ActiveRecord::Migration[7.1]
   def change
     create_table :wishlists do |t|
       t.references :user, null: false, foreign_key: true
+      t.references :book, null: false, foreign_key: true
 
       t.timestamps
     end
