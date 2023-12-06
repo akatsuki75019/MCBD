f20eee5af453b20072fb5971447db1c8233a768e
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-12-06 18:41:57 +0100

    add after create and welcome_send into user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,15 @@
 class User < ApplicationRecord
-
   has_one :cart
   has_one :wishlist
   has_many :orders
   has_many :attendances
   has_many :events, through: :attendances
 
-  after_create :create_user_cart
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  private
-
-  def create_user_cart
-    Cart.create(user: self)
-  end
-  
   validates :password, 
     presence: true, 
     length: { minimum: 6, message: "Votre mot de passe doit contenir à minima 6 caractères (chiffres et/ou lettres)" }
@@ -30,5 +20,20 @@ class User < ApplicationRecord
     format: { 
       #mon email ne peut contenir que des lettres non accentuées, des chiffres et/ou "_" + @ + idem + "." + 3 lettres 
       with: /\A[\p{L}0-9.!~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z]{2,4})\z/, message: "L'email renseigné est incorrect"
-    } 
+    }
+  
+  after_create do
+    create_user_cart 
+    welcome_send
+  end
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
+  private
+
+  def create_user_cart
+    Cart.create(user: self)
+  end
 end
