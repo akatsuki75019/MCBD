380100f3ddb813ecd3629050985efa464a23bb70
nellotte <nelly.guerin@gmail.com>
2023-12-06 15:12:02 +0100

    blocage ajout livre si quantité insuffisante dans le model et gestion des messages d'erreur dans le controller

--- app/controllers/join_table_cart_book_controller.rb
+++ app/controllers/join_table_cart_book_controller.rb
@@ -4,10 +4,17 @@ class JoinTableCartBookController < ApplicationController
     quantity = params.fetch(:quantity, 1).to_i  # prendre la quantité fournie, sinon par défaut à 1
     result = current_user.cart.add_book_in_cart(book, quantity) #la methode add_book_in_cart se trouve dans le model JointTableCartBook
 
-    if result.is_a?(CartBook) 
-      redirect_to books_path, notice: "Le livre a été ajouté au panier avec succès" #dans le cas ou l'ajout se fait depuis de show_book
+    case result
+    when CartBook #dans les cas où l'ajout au panier se fait depuis le show_book
+      redirect_to books_path, notice: "Le livre a été ajouté au panier avec succès"
+    when "Quantité mise à jour dans le panier"
+      redirect_to books_path, notice: "La quantité a été mise à jour dans le panier"
+    when "La quantité demandée n'est pas disponible"
+      redirect_to books_path, alert: "La quantité demandée n'est pas disponible"
+    when "Le livre est en rupture de stock"
+      redirect_to books_path, alert: "Le livre est actuellement en rupture de stock"
     else
-      redirect_to books_path, notice: "Erreur" #dans le cas ou l'ajout se fait depuis de show_book
+      redirect_to books_path, alert: "Erreur lors de l'ajout au panier"
     end
 
   end
--- app/models/joint_table_cart_book.rb
+++ app/models/joint_table_cart_book.rb
@@ -3,13 +3,21 @@ class JointTableCartBook < ApplicationRecord
   belongs_to :book
 
 
-  #Ajouter une entrée "cart-book" dans la BDD
+  #règles de métiers pour ajouter une entrée "cart-book" dans la BDD
   def add_book_in_cart(book, quantity)
+
+    #vérifier si la quantité demandée est disponible ou est en rupture de stock
+    return "La quantité demandée n'est pas disponible" if quantity > book.quantity
+    return "Le livre est en rupture de stock" if book.quantity.zero?
+
     cart_book = cart_books.find_by(book: book)
     # Vérifie si une entre Cart-Book existe dejà pour l'article spécifié dans le panier.
 
     if cart_book # Si le livre existe déjà dans le panier, mettre à jour la quantité en vérifiant si elle est nil.
       new_quantity = cart_book.quantity.to_i + quantity.to_i
+      # Vérifier à nouveau si la quantité demandée est disponible après la mise à jour
+      return "La quantité demandée n'est pas disponible" if new_quantity > book.quantity
+      
       cart_book.update(quantity: new_quantity)
       return "Quantité mise à jour dans le panier"
     else # Sinon, créez un nouvel enregistrement CartBook pour ce livre dans le panier avec la quantité spécifiée.
