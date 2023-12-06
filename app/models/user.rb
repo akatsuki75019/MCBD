a364df8e1916610c68300bd7915d0e74f476f2bc
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-12-06 18:27:19 +0100

    update email regex into user model so now format is stronger

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,15 @@ class User < ApplicationRecord
     Cart.create(user: self)
   end
   
-  validates :password, presence: true, length: { minimum: 6, message: "Votre mot de passe doit contenir à minima 6 caractères (chiffres et/ou lettres)" }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Attention : Email incorrect" } 
-
+  validates :password, 
+    presence: true, 
+    length: { minimum: 6, message: "Votre mot de passe doit contenir à minima 6 caractères (chiffres et/ou lettres)" }
+  
+  validates :email, 
+    presence: { message: "N'oubliez pas l'email 🤓" },
+    uniqueness: { message: "Un compte possède déjà cet email, veuillez vous connecter" },
+    format: { 
+      #mon email ne peut contenir que des lettres non accentuées, des chiffres et/ou "_" + @ + idem + "." + 3 lettres 
+      with: /\A[\p{L}0-9.!~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z]{2,4})\z/, message: "L'email renseigné est incorrect"
+    } 
 end
