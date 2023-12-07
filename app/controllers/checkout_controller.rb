f2729c10eec3550248d5d99f4a61b2f2ba1a901b
nellotte <nelly.guerin@gmail.com>
2023-12-07 16:09:26 +0100

    finalization Stripe, alimentation BDD Order et Joint_Table_order_book lors de la validation du paiement Stripe

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -40,26 +40,13 @@ class CheckoutController < ApplicationController
 
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-    joint_table_cart_book_ids = @session.metadata.joint_table_cart_book_ids
+    joint_table_cart_book_ids = @session.metadata.joint_table_cart_book_ids.split(',').map(&:to_i)
    
 
-    # Création d'une nouvelle instance dans la BDD Order
-    #@order = Order.create(user_id: current_user.id)
-
-    # Récupération de l'order_id nouvellement créé
-    #@order_id = @order.id
+    #Création d'une nouvelle instance dans la BDD Order (voir dans le model Order)
+    Order.create_order_with_books(current_user, joint_table_cart_book_ids)
 
 
-    
-    # Récupération des IDs des cart_items depuis les métadonnées
-    #cart_item_ids = @session.metadata.cart_item_ids.split(',').map(&:to_i)
-
-
-    #Création des instances dans la table jointe order_items
-    #cart_item_ids.each do |cart_item_id|
-      #cart_item = CartItem.find(cart_item_id)
-      #OrderItem.create(order_id: @order.id, item_id: cart_item.item_id)
-    #end
 
     # Effacer les articles du panier après la commande
       current_user.cart.joint_table_cart_books.destroy_all
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,10 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = current_user.orders.includes(order_items: :item)
+  end
+
+  def create
+  end
+  
 end
