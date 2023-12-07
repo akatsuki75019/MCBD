babc9c6abd75401d4ff1f08546c44c89ec6ce067
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-12-07 16:55:50 +0100

    controller admin application_controller.rb fulfilled and button with restriction add to sidebar, now I've to set up everuthing behind for admin part

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,21 +1,13 @@
-# All Administrate controllers inherit from this
-# `Administrate::ApplicationController`, making it the ideal place to put
-# authentication logic or other before_actions.
-#
-# If you want to add pagination or other controller-level concerns,
-# you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      redirect_to '/', alert: 'Accès non autorisé !' unless current_user && access_whitelist
     end
 
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+    def access_whitelist
+      current_user.is_admin?
+    end
   end
 end
