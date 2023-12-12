3a7d4547310ec488dec24f5c45394d6ee190779c
nellotte <nelly.guerin@gmail.com>
2023-12-12 14:13:27 +0100

    event attendance : ajout bouton participer, création des routes, alimentation du controller et de la methode Attend

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,22 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: [:attend]
 
   # GET /events or /events.json
   def index
     @events = Event.all
+  end
+
+  def attend
+    @event = Event.find(params[:id])
+
+    if current_user.attendances.create(event: @event)
+      flash[:notice] = 'Inscription réussie!'
+    else
+      flash[:alert] = "Erreur lors de l'inscription"
+    end
+
+    redirect_to @event
   end
 
   # GET /events/1 or /events/1.json
