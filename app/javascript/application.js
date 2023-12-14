583bd748dd30e697b1744d9b9123599aa870ae7c
Sam <146119225+Samalzdh@users.noreply.github.com>
2023-12-15 00:35:24 +0100

    correction js push precedent

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Entry point for the build script in your package.json
-import "jquery/src/jquery"
-import "bootstrap/dist/js/bootstrap.bundle"
+
 import "@hotwired/turbo-rails"
 import "./controllers"
-import 'turbo-rails';
