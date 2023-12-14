312ff494e7de7b2d634416c1e2402c42e3efc64b
nellotte <nelly.guerin@gmail.com>
2023-12-14 10:46:44 +0100

    initialisation travaux

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,14 @@ class UserMailer < ApplicationMailer
 
     mail(to: @user.email, subject: "Bienvenue chez nous !")
   end
+
+
+  def order_confirmation(user)
+    #Récupère l'instance pour la view
+    @user = user
+    @url  = 'https://mcbdapp-2089282d029d.herokuapp.com/'
+
+    mail(to: @user.email, subject: "Merci votre achat.")
+  end
+
 end
